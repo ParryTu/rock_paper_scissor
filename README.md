5df03e26bd2c7b58c3631923baead9c99e59d904
Parry <pt987@nyu.edu>
2021-11-27 16:34:53 -0800

    bug fixes, game refinement

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # rock_paper_scissor
 rock paper scissor
+
+Vs the computer in a game of Rock Paper Scissors (first to 5)
+Open the console on your browser and enter "game()"
+Have fun and take breaks when playing for long sessions!
--- game_script.js
+++ game_script.js
@@ -4,7 +4,6 @@ let randomnum = Math.random();
     return "rock"
    } else if (randomnum >= (2/3)){
     return "paper"
-
    } else {
        return "scissors"
    }
@@ -12,14 +11,13 @@ let randomnum = Math.random();
 
 function round(){
     let comp = computerPlay();
-     //input your choice
-     let stdpick = text.toLowerCase(prompt("Pick rock, paper or scissors"));
+     let stdpick = (prompt("Pick rock, paper or scissors")).toLowerCase();
      if (stdpick == comp) {
          return "It's a draw"
      } 
 
      if (comp=="rock"){
-         if (stdpick = "scissors") {
+         if (stdpick == "scissors") {
              return "Computer wins"
          } else {
              return "You win"
@@ -27,20 +25,19 @@ function round(){
      }
 
      if (comp=="paper"){
-        if (stdpick = "scissors") {
+        if (stdpick == "scissors") {
             return "You win"
         } else {
             return "Computer wins"
         }
     }
 
-    if (stdpick ="rock"){
+    if (stdpick =="rock"){
         return "You win"
      } else {
         return "Computer wins"
      }
     
-
 }
 
 
@@ -53,13 +50,15 @@ function game (){
         let rResult = round();
         if (rResult == "Computer wins"){
             compScore++;
-        } elseif (rResult == "You win") {
+        } else if (rResult == "You win") {
             yourScore++;
+        } else {
+            console.log("its a draw");
         }
         console.log("your score is " + yourScore);
         console.log("computer score is " + compScore);
     }
-    if (compScore =5) {
+    if (compScore ==5) {
      return "You lose"
     } else {
      return "you winner"
